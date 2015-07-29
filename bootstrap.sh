9e6d5fa8c51f2cac7e04ac1fe8202fa6d177f01f
Lawrence Wakefield <lawrence@iamlawrence.me>
2015-07-29 11:49:49 +1000

    Fixed symlink, played with i3

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ git clone https://github.com/gmarik/Vundle.vim.git ./vim/vim/bundle/Vundle.vim
 
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sdf ~/dotfiles/vim/vim/ ~/.vim
-ln -sdf ~/dotfiles/i3 ~/.i3
+ln -sdf ~/dotfiles/i3/ ~/.config
